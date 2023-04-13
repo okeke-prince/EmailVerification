1bd8599fe277e7562a509fdf64e8383c29696c61
trikky <princeinme128@gmail.com>
2023-04-13 13:04:20 +0300

    update

--- src/main/java/com/example/demo/EmailVerificationApplication.java
+++ src/main/java/com/example/demo/EmailVerificationApplication.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EmailVerificationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EmailVerificationApplication.class, args);
 	}
 
 }
